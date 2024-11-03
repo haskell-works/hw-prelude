7c9eb2b150a14aee4af4a12f033387032ba40832
John Ky <john.ky@iohk.io>
2024-11-03 12:54:10 +1100

    Delete RenderedError

--- src/HaskellWorks/Error/Types/GenericError.hs
+++ src/HaskellWorks/Error/Types/GenericError.hs
@@ -6,10 +6,6 @@ module HaskellWorks.Error.Types.GenericError (
     GenericError (..),
 ) where
 
-import           Data.Aeson
-import           Data.Generics.Product.Any
-import           HaskellWorks.Error.Types.RenderedError
-import           Lens.Micro
 
 import           HaskellWorks.Prelude
 
@@ -17,10 +13,3 @@ newtype GenericError = GenericError
     { message :: Text
     }
     deriving (Eq, Generic, Show)
-
-instance ToRenderedError GenericError where
-    toRenderedError e =
-        RenderedError
-            { error   = "GenericError"
-            , payload = toJSON $ e ^. the @"message"
-            }
--- src/HaskellWorks/Error/Types/TimedOut.hs
+++ src/HaskellWorks/Error/Types/TimedOut.hs
@@ -6,21 +6,9 @@ module HaskellWorks.Error.Types.TimedOut (
     TimedOut (..),
 ) where
 
-import           Data.Aeson
-import           Data.Generics.Product.Any
-import           HaskellWorks.Error.Types.RenderedError
-import           Lens.Micro
-
 import           HaskellWorks.Prelude
 
 newtype TimedOut = TimedOut
     { message :: Text
     }
     deriving (Eq, Generic, Show)
-
-instance ToRenderedError TimedOut where
-    toRenderedError e =
-        RenderedError
-            { error   = "TimedOut"
-            , payload = toJSON $ e ^. the @"message"
-            }
