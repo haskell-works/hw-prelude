3607e719a0e4d03ded9cbcbd910dba388730858e
John Ky <john.ky@iohk.io>
2024-11-24 11:23:32 +1100

    New orElse function

--- src/HaskellWorks/Either.hs
+++ src/HaskellWorks/Either.hs
@@ -1,4 +1,10 @@
-module HaskellWorks.Data.Either
-  ( eitherToMaybe
-  , maybeToEither
+module HaskellWorks.Either
+  ( orElse
   ) where
+
+import           Data.Either
+import           Data.Function
+
+orElse :: Either e a -> Either e a -> Either e a
+orElse a b =
+  either (const b) Right a
